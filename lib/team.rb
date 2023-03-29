7a09a5cdad7ae48630a95717e96585b4da854c2f
Lauren Golden <llgolden@gmail.com>
2023-03-29 09:58:15 -0600

    Add the completed iteration 2 work

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,20 @@
 class Team
-  # attr_reader :
+  attr_reader :mascot, :city, :roster
 
   def initialize (mascot, city)
     @mascot = mascot
     @city = city
     @roster = []
   end #def initialize
+
+  def player_count
+    roster.length
+  end
+
+  def add_player(player)
+    @roster << player
+  end
+
+
 
 end #final
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,8 +5,18 @@ require './lib/team'
 RSpec.describe Team do
   it 'exists' do
     team = Team.new("Dodgers", "Los Angeles")
-
     expect(team).to be_an_instance_of Team
-
+    expect(team.roster).to eq([])
+    expect(team.player_count).to eq(0)
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.roster
+    team.player_count
+    expect(team.player_count).to eq(2)
   end
+
+  
 end #final
+# expect().to eq()
