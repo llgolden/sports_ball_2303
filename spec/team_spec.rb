f308143dde8e56c365e79fc168091e2de339a1fe
Lauren Golden <llgolden@gmail.com>
2023-03-29 10:02:09 -0600

    Set up iteration 3

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,7 +3,7 @@ require './lib/player'
 require './lib/team'
 
 RSpec.describe Team do
-  it 'exists' do
+  it '-eration 2' do
     team = Team.new("Dodgers", "Los Angeles")
     expect(team).to be_an_instance_of Team
     expect(team.roster).to eq([])
@@ -17,6 +17,20 @@ RSpec.describe Team do
     expect(team.player_count).to eq(2)
   end
 
-  
+  it '-eration 3' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+    # team.long_term_players
+    
+  end
+
+
 end #final
 # expect().to eq()
