3a623c055d6440a3897ffefdf2a5511fe0442cdd
Lauren Golden <llgolden@gmail.com>
2023-03-29 09:48:00 -0600

    Add team ruby and spec files

---
+++ lib/team.rb
@@ -0,0 +1,10 @@
+class Team
+  # attr_reader :
+
+  def initialize (mascot, city)
+    @mascot = mascot
+    @city = city
+    @roster = []
+  end #def initialize
+
+end #final
---
+++ spec/team_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/player'
+require './lib/team'
+
+RSpec.describe Team do
+  it 'exists' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team).to be_an_instance_of Team
+
+  end
+end #final
